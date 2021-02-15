d93fc3d86eaa42139efeb5fd3f83167754290f0e
Scott <scott.morris@matrix.ai>
2021-02-15 14:49:43 +1100

    Linted and fixed

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,14 +1,18 @@
 import Logger from '../src/Logger';
 
-jest
-  .useFakeTimers('modern')
-  .setSystemTime(new Date('2020-01-01').getTime());
+jest.useFakeTimers('modern').setSystemTime(new Date('2020-01-01').getTime());
 
 describe('index', () => {
   test('Testing the output of the logger', () => {
     const consoleSpy = jest.spyOn(console, 'error');
     const logger = new Logger();
     logger.info('Test output');
-    expect(consoleSpy).toHaveBeenCalledWith("1/1/2020 @ 11:0:0", ": ", "root", ": ", "Test output");
+    expect(consoleSpy).toHaveBeenCalledWith(
+      '1/1/2020 @ 11:0:0',
+      ': ',
+      'root',
+      ': ',
+      'Test output',
+    );
   });
 });
