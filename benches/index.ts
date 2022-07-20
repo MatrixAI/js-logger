1cc50c78b82d87a9a8c010ea7796cd4f3958b78a
Roger Qiu <roger.qiu@polyhack.io>
2022-07-21 02:35:58 +1000

    ci: integrated benchmarking into build jobs

--- benches/index.ts
+++ benches/index.ts
@@ -16,13 +16,34 @@ async function main(): Promise<void> {
   await loggerHierarchy();
   await loggerFiltered();
   await loggerHandlers();
+  const resultFilenames = await fs.promises.readdir(
+    path.join(__dirname, 'results'),
+  );
+  const metricsFile = await fs.promises.open(
+    path.join(__dirname, 'results', 'metrics.txt'),
+    'w',
+  );
+  let concatenating = false;
+  for (const resultFilename of resultFilenames) {
+    if (/.+_metrics\.txt$/.test(resultFilename)) {
+      const metricsData = await fs.promises.readFile(
+        path.join(__dirname, 'results', resultFilename),
+      );
+      if (concatenating) {
+        await metricsFile.write('\n');
+      }
+      await metricsFile.write(metricsData);
+      concatenating = true;
+    }
+  }
+  await metricsFile.close();
   const systemData = await si.get({
     cpu: '*',
     osInfo: 'platform, distro, release, kernel, arch',
     system: 'model, manufacturer',
   });
   await fs.promises.writeFile(
-    'benches/results/system.json',
+    path.join(__dirname, 'results', 'system.json'),
     JSON.stringify(systemData, null, 2),
   );
 }
